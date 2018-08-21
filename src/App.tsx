705e0978d6dda7189551e61f4d363933875566da
Ian Tan <ian.tan@techinasia.com>
2018-08-21 18:21:22 +0800

    feat(style): add custom theme matching Zilliqa's livery

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,29 @@ import styled from 'styled-components';
 import Navigator from './components/Navigator';
 import ScillaEditor from './components/Editor';
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#7cf4f3',
+      main: '#42c1c0',
+      dark: '#009090',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#2e4cb4',
+      main: '#6977e7',
+      dark: '#9ea6ff',
+      contrastText: '#000000',
+    },
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        background: 'linear-gradient(45deg, #42c1c0 30%, #6977e7 90%)',
+      },
+    },
+  },
+});
 
 const Wrapper = styled.div`
   flex-grow: 1;
