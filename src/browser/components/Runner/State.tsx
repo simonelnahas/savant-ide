d8b2e29273184b4e1ec2ea9142776dd1a03dc964
Ian Tan <iantanwx@gmail.com>
2018-09-07 12:17:19 +0800

    feat(state): enable deserialisation for stateLog

--- src/browser/components/Runner/State.tsx
+++ src/browser/components/Runner/State.tsx
@@ -76,6 +76,7 @@ export default class StateTree extends React.Component<Props, State> {
             ...contracts[address],
             init: deserialiseContractState(contracts[address].init),
             state: deserialiseContractState(contracts[address].state),
+            stateLog: contracts[address].stateLog.map((state) => deserialiseContractState(state)),
           },
         };
       }, {});
