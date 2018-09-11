6d9791ffdd2da95cf6708af0858cf83490192c7a
Ian Tan <iantanwx@gmail.com>
2018-09-11 15:26:27 +0800

    fix(server): always create temp folder if it does not exist

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,13 +1,22 @@
-import express, { Handler, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import express, { Handler, Request, Response, NextFunction } from 'express';
+import fs from 'fs';
 import lusca from 'lusca';
+import path from 'path';
 import { check, run } from './handlers';
 
 const app = express();
 const wrapAsync = (fn: Handler) => (req: Request, res: Response, next: NextFunction) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
+
+// create temp folder if it doesn't exist
+const temp = path.join(process.cwd(), 'temp');
+
+if (!fs.existsSync(temp)) {
+  fs.mkdirSync(temp);
+}
 
 // configure express
 app.set('port', process.env.PORT || 8080);
