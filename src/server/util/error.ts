336284ba0df28704cc0115f721f2794afb1689cc
Ian Tan <iantanwx@gmail.com>
2018-09-17 14:26:49 +0800

    chore(): add banner to all source files

--- src/server/util/error.ts
+++ src/server/util/error.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2018 Zilliqa
+ * This source code is being disclosed to you solely for the purpose of your participation in 
+ * testing Zilliqa. You may view, compile and run the code for that purpose and pursuant to 
+ * the protocols and algorithms that are programmed into, and intended by, the code. You may 
+ * not do anything else with the code without express permission from Zilliqa Research Pte. Ltd., 
+ * including modifying or publishing the code (or any part of it), and developing or forming 
+ * another public or private blockchain network. This source code is provided ‘as is’ and no 
+ * warranties are given as to title or non-infringement, merchantability or fitness for purpose 
+ * and, to the extent permitted by law, all liability for your use of the code is disclaimed. 
+ * Some programs in this code are governed by the GNU General Public License v3.0 (available at 
+ * https://www.gnu.org/licenses/gpl-3.0.en.html) (‘GPLv3’). The programs that are governed by 
+ * GPLv3.0 are those programs that are located in the folders src/depends and tests/depends 
+ * and which include a reference to GPLv3 in their program files.
+ */
+
 const TYPE_ERR_RE = new RegExp(/.*\[(?:.*)?\:([0-9]+\:[0-9]+)\]\:? ((.*)\n?(.*)?)$/);
 const SYN_ERR_RE = new RegExp(/^Syntax error.*line ([0-9]+)\, position ([0-9]+)/);
 const GAS_ERR_RE = new RegExp(/Ran out of gas/g);
