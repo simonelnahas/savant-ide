fc0e6e38669feab9c5b41b5ee1710cc24a1ee806
Ian Tan <iantanwx@gmail.com>
2019-02-18 11:15:33 +0800

    feat(cicd): add automatic ecs deployment

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -5,18 +5,26 @@ set -e
 docker --version
 aws --version
 
-[ -n "$1" ] && jobs=$1 || jobs=2
-[ -z ${TRAVIS_BUILD_DIR} ] && DOCKER_DIR=. || DOCKER_DIR=${TRAVIS_BUILD_DIR}
+[ -z "$TRAVIS_BUILD_DIR" ] && DOCKER_DIR=. || DOCKER_DIR=$TRAVIS_BUILD_DIR
 [[ -z "$TRAVIS_COMMIT" ]] && TRAVIS_COMMIT=HEAD
 echo $TRAVIS_COMMIT
 commit=$(git rev-parse --short=7 ${TRAVIS_COMMIT})
 account_id=$(aws sts get-caller-identity --output text --query 'Account')
 region_id=us-west-2
 registry_url=${account_id}.dkr.ecr.${region_id}.amazonaws.com/scilla-runner-api
+# ECS settings
+cluster=scilla-runner-api
+service=scilla-runner-api
 
 echo "Building container..."
-eval $(aws ecr get-login --no-include-email --region ${region_id})
-docker build -t ${registry_url}:latest -t ${registry_url}:${commit} ${DOCKER_DIR}
+eval "$(aws ecr get-login --no-include-email --region $region_id)"
+docker build -t "$registry_url:latest" -t "$registry_url:$commit" "$DOCKER_DIR"
 
 echo "Pushing to ECR..."
-docker push ${registry_url}
+docker push "$registry_url"
+
+echo "Deploying to ECS..."
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | sudo tee /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
+
+ecs-deploy -c $cluster -n $service -i "$registry_url:latest"
